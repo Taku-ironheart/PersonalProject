be9c512d10ec4fbe8538822d551d3ab48c19afc2
Taku-ironheart <get.latest.information@gmail.com>
2021-02-06 18:21:42 +0900

    3rd commit

---
+++ temp_hum_rev2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import requests
+import grovepi
+import math
+
+url = "https://notify-api.line.me/api/notify" 
+token = "KSrZELTc6hzCPOhyhRGj2eEvR4GgnswejJLDYhY0ieP"
+headers = {"Authorization" : "Bearer "+ token} 
+message =  "ラズパイからメッセージを受信しました！" 
+payload = {"message" :  message} 
+r = requests.post(url, headers = headers, params=payload)
+
+
+# Connect the Grove Temperature & Humidity Sensor Pro to digital port D4
+# This example uses the blue colored sensor.
+# SIG,NC,VCC,GND
+sensor = 4  # The Sensor goes on digital port 4.
+
+# temp_humidity_sensor_type
+# Grove Base Kit comes with the blue sensor.
+blue = 0    # The Blue colored sensor.
+white = 1   # The White colored sensor.
+
+[temp,humidity] = grovepi.dht(sensor,blue)
+print(temp)
+print(humidity)
